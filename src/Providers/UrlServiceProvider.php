0fee7900527528dcfd6f0c0a179253efffa4fa39
Kewan <kewanshunn@gmail.com>
2019-04-12 09:40:20 +1000

    Add url singleton to UrlServiceProvider

--- src/Providers/UrlServiceProvider.php
+++ src/Providers/UrlServiceProvider.php
@@ -2,13 +2,38 @@
 
 namespace Kewan\SessionInUrl\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+use Kewan\SessionInUrl\Routing\UrlGenerator;
 
 class UrlServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(UrlGenerator::class, \Kewan\SessionInUrl\Routing\UrlGenerator::class);
+        $this->app->bind(\Illuminate\Routing\UrlGenerator::class, UrlGenerator::class);
+
+        $this->app->singleton('url', function ($app) {
+            $routes = $app['router']->getRoutes();
+
+            // The URL generator needs the route collection that exists on the router.
+            // Keep in mind this is an object, so we're passing by references here
+            // and all the registered routes will be available to the generator.
+            $this->app->instance('routes', $routes);
+            $url = new UrlGenerator($routes, $app->rebinding('request', function ($app, $request) {
+                    $app['url']->setRequest($request);
+                }));
+            $url->setSessionResolver(function () {
+                return $this->app['session'];
+            });
+
+            // If the route collection is "rebound", for example, when the routes stay
+            // cached for the application, we will need to rebind the routes on the
+            // URL generator instance so it has the latest version of the routes.
+            $app->rebinding('routes', function ($app, $routes) {
+                $app['url']->setRoutes($routes);
+            });
+
+            return $url;
+        });
     }
+}
 }
